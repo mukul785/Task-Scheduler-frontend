6bef39efaa50adb149809b8e9c04f988e01c1646
mukul785 <156766802+mukul785@users.noreply.github.com>
2024-10-14 14:09:28 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Home from './components/home';
